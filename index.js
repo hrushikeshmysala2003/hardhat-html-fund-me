1e445a2916db1fabbf7ed4dd36d090d7d378ce7c
rushi <hrushikeshmysala2003@gmail.com>
2023-10-27 12:00:03 +0530

    Implemented fund function and understood async nature

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { ethers } from "./ethers-5.1.esm.min.js";
-
+import { abi, contractAddress } from "./constants.js";
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fund");
 connectButton.onclick = connect;
@@ -16,11 +16,40 @@ async function connect() {
   }
 }
 
-async function fund(ethAmount) {
+async function fund() {
+  const ethAmount = "0.5";
   console.log(`Funding with ${ethAmount}`);
   if (typeof window.ethereum !== "undefined") {
     // provide / connection to the blockchain
     // signer / wallet / someone with some gas
     // contract that we are interacting with ABI and address
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      });
+      await listenForTransactionMine(transactionResponse, provider);
+      console.log("Done!");
+      //   listen for the tx to be mined
+      // listen for an event - we havent learned about yet
+    } catch (error) {
+      console.log(error);
+    }
   }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}...`);
+  // return new Promise()
+  // listen for this transaction to finish
+  return new Promise((resolve, reject) => {
+    provider.once(transactionResponse.hash, (TransactionReceipt) => {
+      console.log(
+        `Completed with ${TransactionReceipt.confirmations} confirmations`
+      );
+      resolve();
+    });
+  });
 }
